756cffc6a083337b790f5fe9cdcea850564b2dcc
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-07-14 11:35:42 -0400

    Time: 187 ms (16.43%), Space: 47 MB (21.48%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+/*
+    A
+        -Loop through the array
+        -the element is not 0 
+        -Splice the element that is zero
+        -
+*/
+var moveZeroes = function(nums) {
+   let size = nums.length;
+     for (let i = 0; i < size; i++) {
+         if (nums[i] === 0) {
+             nums.splice(i, 1);
+             nums.push(0);
+             i--;
+             size--;
+         }
+     }
+    return nums;
+};
