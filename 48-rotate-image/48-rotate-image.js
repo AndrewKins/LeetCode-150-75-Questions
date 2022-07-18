2d02faa5b8e1d91d3dd6ae19834eb0eebf83ea5e
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-07-18 11:02:13 -0400

    Time: 110 ms (16.92%), Space: 42.4 MB (26.19%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -3,10 +3,10 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    for(let r = 0; r < matrix.length; r++){ //loop through the rows (top to down)
-                                            //columns or left to right
+    for(let r = 0; r < matrix.length; r++){ //loop through the rows (top to bottom)
+                                            //columns are left to right
         for(let c = r; c < matrix[0].length; c++){
-            [matrix [r][c], matrix[c][r]] = [matrix [c][r], matrix[r][c]]
+            [matrix [r][c], matrix[c][r]] = [matrix [c][r], matrix[r][c]]        
         }
     }
     for(let row of matrix){
