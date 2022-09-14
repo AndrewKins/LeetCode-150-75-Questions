2d0ee34f68b3499f7f087b4f59f220fd780edc30
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-09-13 20:44:10 -0400

    Time: 155 ms (35.72%), Space: 45.8 MB (97.18%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,27 +3,12 @@
  * @return {boolean}
  */
 /*
-P -
-    Positioned at the arrays first index. (0)
-    Each element represents max jump length at that positons.
-    Return true if you can reacht the last index.
-    False if otherwise
-E
-D - Array
-A
-    -Initialize a last index variable to be the last index
-    -iterate backwards
-        -if the current index + the currNum >= the last index
-            reassign the last index to be i
-            return last index === 0
-C
 */
 var canJump = function(nums) {
-    let prevIndex = nums.length - 1
-    for(let i = nums.length; i >= 0; i--){
-        if(i + nums[i] >= prevIndex){
-            prevIndex = i
-        }
-    }
-    return prevIndex === 0
+  let maxJump = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    if (i > maxJump) return false;
+    maxJump = Math.max(maxJump, i + nums[i]);
+  }
+  return true;
 };
