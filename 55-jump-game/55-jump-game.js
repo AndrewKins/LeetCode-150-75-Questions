390b952ea40a5320231e1b58743df7e7a3a5a70c
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-09-28 15:08:11 -0400

    Time: 111 ms (70.13%), Space: 46.4 MB (58.78%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,6 +3,25 @@
  * @return {boolean}
  */
 /*
+P
+    -Given an array of integers
+    -Starting at the first index, grabbing the value of that element
+    -Seeing if you can jump to the max length of the array
+E
+    input: [2,1,4,7,3]
+    output: [false]
+    input:[1,1,1,2]
+    output:true
+D
+    -Arrays
+A
+    -Initialize a variable that will keep track of the maximum steps you can take.
+    -Loop through the given input
+    -If the current index is greater than the maxJump return false
+    -Reassign maxjump to the current current largest jump 
+    -maxJump or the currentindex + the element value
+    -return true
+C
 */
 var canJump = function(nums) {
   let maxJump = 0;
