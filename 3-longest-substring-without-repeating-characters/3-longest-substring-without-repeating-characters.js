6e6b67bd6d0cd09cca5470cae27e0154129017ea
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-09-28 10:58:09 -0400

    Time: 140 ms (64.38%), Space: 47.1 MB (67.13%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,21 +2,47 @@
  * @param {string} s
  * @return {number}
  */
+/*
+P
+    -Find the length of the longest substring without repeating characters.
+E
+    input: "yyyyyyy"
+    output: 1
+D
+    -Arrays / Objects
+A
+    -Initialize a variable that will keep track of the start of the substring
+    -Initialize a variable that will keep track of the end of the substring
+    -Initialize a variable that will keep track of the maxLength of the string.
+    -Initialize a Set to check for duplicate values.
+    -while we havent reached the end of our string
+        -Check our set for a duplicate value
+            -If our set has a duplicate value 
+                -Then we can delete starting char (started from 0 index) - 
+                    -we can then start from the next element in the given string(move starting index from 0 to 1)
+    -If we dont have a duplicate value
+        -Add the value to our set
+        -MaxLength to equal the maxLength, Current size of the string
+        -increment the end of our substring
+    
+-Return max Length
+C
+*/
 var lengthOfLongestSubstring = function(s) {
-    const found = new Map();
     let start = 0;
-    // keeps track of the starting index of the current substring.
+    let end = 0;
     let maxLength = 0;
-     // keeps track of the maximum substring length.
+    let foundCharacters = new Set();
     
-    for(let i = 0; i < s.length; i++) {
-        // if the current char was seen, move the start to (1 + the last index of this char)
-        // max prevents moving backward, 'start' can only move forward
-        if(found.has(s[i])) start = Math.max(found.get(s[i]) + 1, start)
-        found.set(s[i], i);
-        // maximum of the current substring length and maxLen
-        maxLength = Math.max(i - start + 1, maxLength);
-    } 
-    
-    return maxLength;  
+    while(end < s.length){
+        if(foundCharacters.has(s[end])){ //check for duplicate, if found, delete
+            foundCharacters.delete(s[start])
+            start += 1 // moving starting index one character forward
+        } else {
+            foundCharacters.add(s[end])
+            maxLength = Math.max(maxLength, foundCharacters.size)
+            end += 1
+        }
+    }
+    return maxLength
 };
