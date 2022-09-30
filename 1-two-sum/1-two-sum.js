bbbd1295e6ec4fd34dd4c4cd1743287f3c276557
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-09-30 17:49:15 -0400

    Time: 136 ms (47.26%), Space: 45 MB (8.94%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,36 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-//REFACTORED
-//Using HashMap - Check for matching pairs
-    
-//Declare an empty object to that will contain the array values
-//Initilize the key value pairs with a value of 1
-// 
-var twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
     let hashMap = {}
-    for(let ele in nums){
-        console.log(ele)
-        let matchingPair = target - nums[ele];
-        if(hashMap[matchingPair]) {
-            return [hashMap[matchingPair], ele];
-        } else {
-            hashMap[nums[ele]] = ele
-        }
+    for(ele in nums){
+        let matchingPairs = target - nums[ele]; //9 - 2
+        if(hashMap[matchingPairs]){ //hashmap contains remainder above
+            return [hashMap[matchingPairs], ele] //return number and index
+        } else
+        hashMap[nums[ele]] = ele
     }
 }
-
-//indices = index
-//return indexes of 2 numbers so they add up to the target 
-//given an array of integers [2,4,6,8]
-//return indices of two numbers so they add up to the target []
-
-
-//Loop through the array
-//If the sum of the first number and the second number added together is the target number return the indexes of those numbers.
-//If the first number plus the second number (sum) isnt the target total
-
-
-
-//possible edge cases
-//What if the first two numbers aren't equal to the target number?
