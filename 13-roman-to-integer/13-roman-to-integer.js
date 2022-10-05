43543627d98ad3f5667ef423ed81793fa0f96543
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-10-05 09:43:29 -0400

    Time: 205 ms (62.38%), Space: 47.1 MB (68.87%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -25,13 +25,15 @@ var romanToInt = function(s) {
     let hashMap = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
    let result = 0;
     for (let i = 0; i < s.length; i++) {
+        const currEle = hashMap[s[i]]
+        const nextEle = hashMap[s[i+1]]
         //if the next roman numeral is larger, then we know we have to subtract this number
-        if (hashMap[s[i]] < hashMap[s[i+1]]) {
-            result-=hashMap[s[i]]
+        if (currEle < nextEle) {
+            result -= currEle
         } 
         //otherwise, add like normal. 
         else {
-            result+=hashMap[s[i]]
+            result += currEle
         }
     }
     return result
