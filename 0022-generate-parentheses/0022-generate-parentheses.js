5d2b53f329f882fe15a1b7f10e081686cb84dfe5
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-10-18 10:48:13 -0400

    Time: 102 ms (47.31%), Space: 42.4 MB (85.24%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = (n) => {
+    const output = []
+    const helper = (str, openCount, closeCount) => {
+        if(closeCount === n){
+            output.push(str)
+        }
+        if(openCount < n){
+            helper(str + '(', openCount + 1, closeCount)
+        }
+        if(openCount > closeCount){
+            helper(str + ')', openCount, closeCount + 1)
+        }
+    }
+    helper("",0,0)
+    return output
+};
