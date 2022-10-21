bb5946ecaff5fdba087d62124a3a58530a138f1f
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-10-21 13:34:38 -0400

    Time: 133 ms (16.31%), Space: 45.3 MB (31.87%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = (nums) =>{
+    let hashMap = {}
+    for(n of nums ){
+        if(hashMap[n]) hashMap[n] += 1
+        else (hashMap[n] = 1)
+        if(hashMap[n] > nums.length / 2) return n
+    } 
+};
