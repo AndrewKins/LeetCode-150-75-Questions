ff5fab9126516163cc53fe3071b2984ddec4d956
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-10-31 16:50:51 -0400

    Time: 119 ms (22.01%), Space: 45.7 MB (5.88%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,14 +4,8 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i = 0; i < nums.length; i += 1 ){
-        let temp = nums[i]
-        
         for(let ele in nums){
-        if(nums[ele] === target){
-            return ele
+        if(nums[ele] === target) return ele
         }
-    }
-}
     return -1
 };
