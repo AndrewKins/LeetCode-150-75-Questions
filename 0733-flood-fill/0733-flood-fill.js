846a8de407bbd86216bb6bf8be28fa83fb7c8546
Andrew Enoe <68964032+AndrewKins@users.noreply.github.com>
2022-11-08 20:58:00 -0500

    Time: 84 ms (87.25%), Space: 43.7 MB (95.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,24 +6,27 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, newColor) {
-    //if checking if the image is null or if a position already has a new color
-    //if so there is nothing to do so return the image.
-    if(image == null || image.length < 1 || image[sr][sc] == newColor){
-        return image;
+    //check for null values image
+    //dfs using recursion
+    //check for out of bounds
+    
+    //check if there are positions to check or already colored in
+    if(image === null || image.length < 1 || image[sr][sc] === newColor){
+        return image
     }
+    
     const initialImage = image[sr][sc];
-    //Below create recursive function in order to fill in selected position with new color.
-    //checking if its out of bounds. 
-        const fill = (image, r, c, newColor, initialImage) => {
-        if(r < 0 || r >= image.length || c < 0 || c >= image[0].length || initialImage != image[r][c]){
-                return;
-            }
+    
+    const fill = (image,r,c,newColor, initalImage) => {
+        if(r < 0 || r >= image.length || c < 0 || c >= image[0].length || initialImage !== image[r][c]){
+            return;
+        }
         image[r][c] = newColor;
-        fill(image,r-1,c,newColor,initialImage);
-        fill(image,r+1,c,newColor,initialImage);
-        fill(image,r,c-1,newColor,initialImage);
-        fill(image,r,c+1,newColor,initialImage);
+        fill(image,r-1,c,newColor, initialImage);
+        fill(image,r+1,c,newColor, initialImage);
+        fill(image,r,c-1,newColor, initialImage);
+        fill(image,r,c+1,newColor, initialImage);
     }
-        fill(image,sr,sc,newColor, initialImage)
-    return image; 
+        fill(image, sr, sc, newColor, initialImage)
+    return image
 };
